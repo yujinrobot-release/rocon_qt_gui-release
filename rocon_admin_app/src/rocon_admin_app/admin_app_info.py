36d52dbe349233f5ce14845904c252ef9e0b171a
Jihoon Lee <jihoonlee.in@gmail.com>
2014-11-29 15:57:27 +0900

    Revert "Trimmed the branch to only the rocon_qt_master_info sub directory"
    This reverts commit 7983af145386a99229a9d294c754336801ebc288.

---
+++ rocon_admin_app/src/rocon_admin_app/admin_app_info.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/rocon_qt_gui/license/LICENSE
+#
+import rospy
+
+import concert_msgs.msg as concert_msgs
+
+##############################################################################
+# Graph
+##############################################################################
+
+
+class AdminAppInfo(object):
+    def __init__(self):
+        self._event_callback = None
+
+        self.service_list = {}
+        # should make use of concert_msgs/Strings here.
+        rospy.Subscriber("/concert/services/list", concert_msgs.Services, self.update_service_list)
+
+    def _reg_event_callback(self, func):
+        self._event_callback = func
+
+    def update_service_list(self, data):
+        print "update_service_list start"
+        self.service_list = {}
+        for k in data.services:
+            service_name = k.name
+            self.service_list[service_name] = {}
+            self.service_list[service_name]['name'] = service_name
+            self.service_list[service_name]['resource_name'] = k.resource_name
+            self.service_list[service_name]['description'] = k.description
+
+            self.service_list[service_name]['author'] = k.author
+            self.service_list[service_name]['priority'] = k.priority
+            self.service_list[service_name]['launcher_type'] = k.launcher_type
+            self.service_list[service_name]['launcher'] = k.launcher
+            self.service_list[service_name]['uuid'] = k.uuid
+            self.service_list[service_name]['status'] = k.status
+            self.service_list[service_name]['enabled'] = k.enabled
+            # not implementation
+            self.service_list[service_name]['client_list'] = {}
+
+            #html
+            service_context = "<html>"
+            service_context += "<p>-------------------------------------------</p>"
+            service_context += "<p><b>name: </b>" + k.name + "</p>"
+            service_context += "<p><b>resource_name: </b>" + k.resource_name + "</p>"
+            service_context += "<p><b>description: </b>" + k.description + "</p>"
+            service_context += "<p><b>author: </b>" + k.author + "</p>"
+            service_context += "<p><b>priority: </b>" + str(k.priority) + "</p>"
+            service_context += "<p><b>launcher_type: </b>" + k.launcher_type + "</p>"
+            service_context += "<p><b>launcher: </b>" + k.launcher + "</p>"
+            service_context += "<p><b>uuid: </b>" + str(k.uuid) + "</p>"
+            service_context += "<p><b>status: </b>" + str(k.status) + "</p>"
+            service_context += "<p><b>enabled: </b>" + str(k.enabled) + "</p>"
+            service_context += "</html>"
+
+            self.service_list[service_name]['context'] = service_context
+
+        if self._event_callback != None:
+            self._event_callback()
